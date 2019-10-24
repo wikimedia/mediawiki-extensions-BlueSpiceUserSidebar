0cff187adecf0364b3493d8d03bee055f0b8b0fe
dvogel <daniel.vogel@hallowelt.com>
2019-10-24 19:08:53 +0200

    Make collapse state for panels inside usersidebar configurable
    [3.2]
    
    erm#16867
    
    Please cherry-pick to master
    
    Change-Id: Icf30906a0ffe49ce15eeca74aed519614ddc94ea

--- src/Panel/UserSidebarNav.php
+++ src/Panel/UserSidebarNav.php
@@ -50,6 +50,9 @@ class UserSidebarNav extends PanelContainer {
 					if ( isset( $links['params'] ) ) {
 						$params = $links['params'];
 					}
+					$params['panelId'] = $sectionId;
+					$params['panelIdPrefix'] = "bs-usersidebar-";
+
 					$widgetPanel = call_user_func_array( $links['callback'], [ $this->skintemplate, $params ] );
 					if ( $widgetPanel instanceof IPanel ) {
 						$panels[$sectionId] = $widgetPanel;
@@ -58,7 +61,7 @@ class UserSidebarNav extends PanelContainer {
 				continue;
 			}
 
-			$panels[$sectionId] = new CollapsibleLinks( $this->skintemplate, $section, $links );
+			$panels[$sectionId] = new CollapsibleLinks( $this->skintemplate, $section, $links, $sectionId );
 		}
 		return $panels;
 	}
