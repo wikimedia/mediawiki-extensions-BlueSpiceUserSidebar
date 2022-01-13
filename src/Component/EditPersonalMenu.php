af1049e459fd7ca528b6a26e4aff9096ec966dd9
miriam schlindwein <schlindwein@hallowelt.com>
2022-01-13 11:50:38 +0100

    Add edit link for personal menu
    ERM27216
    
    [4.1.x]
    
    Change-Id: Ic6c6381b50a0f4ef1c88dbc35e18772b7ffa163f
    (cherry picked from commit 1319c6b6734c2f2fbd84c2e58e99a6689e62f0de)

---
+++ src/Component/EditPersonalMenu.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace BlueSpice\UserSidebar\Component;
+
+use Message;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
+use Title;
+
+class EditPersonalMenu extends RestrictedTextLink {
+
+	/**
+	 * @var Title
+	 */
+	protected $userSidebarTitle = '';
+
+	/**
+	 * @param Title $userSidebarTitle
+	 */
+	public function __construct( $userSidebarTitle ) {
+		$this->userSidebarTitle = $userSidebarTitle;
+		parent::__construct( [] );
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getId(): string {
+		return 'mm-usersidebar-edit';
+	}
+
+	/**
+	 *
+	 * @inheritDoc
+	 */
+	public function getRequiredRLStyles(): array {
+		return [ "ext.blueSpice.userSidebar.menu.styles" ];
+	}
+
+	/**
+	 *
+	 * @return string[]
+	 */
+	public function getPermissions(): array {
+		return [ 'edit' ];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHref(): string {
+		return $this->userSidebarTitle->getEditURL();
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return Message::newFromKey( 'bs-edit-user-sidebar-link-text' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		return Message::newFromKey( 'bs-edit-user-sidebar-link-title' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getAriaLabel(): Message {
+		return Message::newFromKey( 'bs-edit-user-sidebar-link-text' );
+	}
+
+}
