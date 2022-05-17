3814a03efaee2f2cb25e7d43a60ae7747fbb9739
d.savuljesku <d.savuljesku@gmail.com>
2022-05-17 16:21:32 +0200

    MenuEditor integration
    [4.2]
    
    ERM 27548
    
    Change-Id: I2cb6f9bd3a7e2216648957633135c2689e27b638
    (cherry picked from commit a2c99caa9f3497ffa26cdbc535438c8f083e32f3)

---
+++ resources/menu/UserSidebarTree.js
@@ -0,0 +1,20 @@
+ext = ext || {};
+ext.usersidebar = ext.usersidebar || {};
+ext.usersidebar.menu = {};
+
+ext.usersidebar.menu.UserSidebarTree = function ( cfg ) {
+	ext.usersidebar.menu.UserSidebarTree.parent.call( this, cfg );
+};
+
+OO.inheritClass( ext.usersidebar.menu.UserSidebarTree, ext.menueditor.ui.data.tree.MediawikiSidebarTree );
+
+ext.usersidebar.menu.UserSidebarTree.prototype.getPossibleNodesForLevel = function( lvl ) {
+	switch ( lvl ) {
+		case 0:
+			return [ 'menu-raw-text', 'menu-keyword' ];
+		case 1:
+			return [ 'menu-two-fold-link-spec', 'menu-keyword' ];
+		default:
+			return [];
+	}
+};
