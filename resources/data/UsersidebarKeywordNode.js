176beeb8fa3b6c866432e7bafe23b62f29b27357
miriam schlindwein <schlindwein@hallowelt.com>
2022-07-22 09:42:22 +0200

    Fix keywords in usersidebar
    Depends-On: I2181c23fac00c4cfc5236b723256228dd346592b
    
    ERM29184
    
    [4.2.x]
    
    Change-Id: Icc02a7211c6d57284002b85777df588c3fc32e5a

---
+++ resources/data/UsersidebarKeywordNode.js
@@ -0,0 +1,27 @@
+ext = ext || {};
+ext.usersidebar = ext.usersidebar || {};
+ext.usersidebar.data = {};
+
+ext.usersidebar.data.UsersidebarKeywordNode = function ( cfg ) {
+	ext.usersidebar.data.UsersidebarKeywordNode.parent.call( this, cfg );
+};
+
+OO.inheritClass( ext.usersidebar.data.UsersidebarKeywordNode, ext.menueditor.ui.data.node.KeywordNode );
+
+ext.usersidebar.data.UsersidebarKeywordNode.prototype.getFormFields = function () {
+	return [
+		{
+			name: 'keyword',
+			type: 'dropdown',
+			options: [
+				{ data: 'PAGESVISITED' },
+				{ data: 'YOUREDITS' }
+			],
+			// eslint-disable-next-line camelcase
+			widget_$overlay: true,
+			required: true,
+			label: mw.message( 'menueditor-ui-form-field-keyword' ).text(),
+			help: mw.message( 'menueditor-ui-menu-keyword-help' ).text()
+		}
+	];
+};
