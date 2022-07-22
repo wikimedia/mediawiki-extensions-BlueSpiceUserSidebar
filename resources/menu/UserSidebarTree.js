707eeccbf5b7a916f82228b342b84fc1a2b5ae07
miriam schlindwein <schlindwein@hallowelt.com>
2022-07-22 09:42:22 +0200

    Fix keywords in usersidebar
    Depends-On: I2181c23fac00c4cfc5236b723256228dd346592b
    
    ERM29184
    
    [4.2.x]
    
    Change-Id: Icc02a7211c6d57284002b85777df588c3fc32e5a

--- resources/menu/UserSidebarTree.js
+++ resources/menu/UserSidebarTree.js
@@ -11,9 +11,9 @@ OO.inheritClass( ext.usersidebar.menu.UserSidebarTree, ext.menueditor.ui.data.tr
 ext.usersidebar.menu.UserSidebarTree.prototype.getPossibleNodesForLevel = function( lvl ) {
 	switch ( lvl ) {
 		case 0:
-			return [ 'menu-raw-text', 'menu-keyword' ];
+			return [ 'menu-raw-text', 'menu-keyword-usersidebar' ];
 		case 1:
-			return [ 'menu-two-fold-link-spec', 'menu-keyword' ];
+			return [ 'menu-two-fold-link-spec', 'menu-keyword-usersidebar' ];
 		default:
 			return [];
 	}
