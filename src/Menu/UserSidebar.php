8c7b4b33678f0491f300c0f9ab584b96f185f0a9
d.savuljesku <d.savuljesku@gmail.com>
2022-10-14 16:21:56 +0200

    Adapt to changes in MenuEditor
    Depends-On: I2891975b10abb14a20bc87ffb04d3bed696ea2c1
    
    [4.3]
    
    ERM 28685
    
    Change-Id: Ie6e812dd0412c102eae6e08f7e19082133fc1a92

--- src/Menu/UserSidebar.php
+++ src/Menu/UserSidebar.php
@@ -3,18 +3,21 @@
 namespace BlueSpice\UserSidebar\Menu;
 
 use BlueSpice\UserSidebar\WidgetFactory;
-use MediaWiki\Extension\MenuEditor\IMenu;
+use MediaWiki\Extension\MenuEditor\Menu\MediawikiSidebar;
 use MediaWiki\Extension\MenuEditor\Node\Keyword;
+use MWStake\MediaWiki\Component\Wikitext\ParserFactory;
 use Title;
 
-class UserSidebar implements IMenu {
+class UserSidebar extends MediawikiSidebar {
 	/** @var WidgetFactory */
 	private $widgetFactory;
 
 	/**
 	 * @param WidgetFactory $widgetFactory
+	 * @param ParserFactory $parserFactory
 	 */
-	public function __construct( WidgetFactory $widgetFactory ) {
+	public function __construct( WidgetFactory $widgetFactory, ParserFactory $parserFactory ) {
+		parent::__construct( $parserFactory );
 		$this->widgetFactory = $widgetFactory;
 	}
 
