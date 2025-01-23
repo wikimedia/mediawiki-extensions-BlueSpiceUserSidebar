9abd2cccf9d2b4ca79a8837d5cd29d3e8767804b
hslater <hamish.slater@hallowelt.com>
2025-01-23 14:56:08 +0100

    Update Config namespace
    ERM38444
    
    [REL1_43, master]

--- src/WidgetFactory.php
+++ src/WidgetFactory.php
@@ -2,8 +2,8 @@
 
 namespace BlueSpice\UserSidebar;
 
-use Config;
 use IContextSource;
+use MediaWiki\Config\Config;
 use MWStake\MediaWiki\Component\ManifestRegistry\IRegistry;
 
 class WidgetFactory {
