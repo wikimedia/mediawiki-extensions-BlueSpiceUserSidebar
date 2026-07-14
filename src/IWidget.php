2264b44ad0e84a6db4135aa5ae675ac922146b7a
Hamish <hamish.slater@hallowelt.com>
2026-07-15 00:07:19 +0700

    phpcs
    Change-Id: I866f718d1e4b75e1177210116d4dae3b8889f847

--- src/IWidget.php
+++ src/IWidget.php
@@ -6,19 +6,16 @@ use MediaWiki\Message\Message;
 
 interface IWidget {
 	/**
-	 *
 	 * @return string
 	 */
 	public function getKey(): string;
 
 	/**
-	 *
 	 * @return Message
 	 */
 	public function getHeaderMessage(): Message;
 
 	/**
-	 *
 	 * @return array
 	 */
 	public function getLinks(): array;
